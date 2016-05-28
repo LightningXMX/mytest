a58b3a1271e7cd6bb288e48cc3d4945129e7cb65
lightningxmx <lightningxmx@163.com>
2016-05-28 13:31:11 +0800

    add

---
+++ src/main/java/work/HelloWorld.java
@@ -0,0 +1,10 @@
+package work;
+
+/**
+ * Created by LIGHTNING on 2016/5/28.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
